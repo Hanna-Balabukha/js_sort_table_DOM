cc3ae8ba71633d88395657c04490b6e8c46ca278
Hanna Balabukha <anna.aniskovets1992@ukr.net>
2025-01-28 17:07:11 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('thead>th');
+
+for (let i = 0; i < headers.length; i++) {
+  const th = headers[i];
+
+  th.addEventListener('click', () => {sortTable(i);
+  });
+}
+
+function sortTable(ind) {
+  const table = document.querySelector('table');
+
+  const rows = table.querySelectorAll('tbody>tr');
+
+  rows.sort(function(rowA, rowB) {
+    const cellA = rowA.cells[ind].textContent;
+    const cellB = rowB.cells[ind].textContent;
+
+    return cellA.localeCompare(cellB);
+  })
+
+  rows.forEach(function(row) {
+    table.querySelector('tbody').appendChild(row);
+  });
+}
