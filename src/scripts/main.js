fa04d687981f699e68d8b205892e94f6009bfed7
Hanna Balabukha <anna.aniskovets1992@ukr.net>
2025-01-29 11:29:13 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,26 @@
 'use strict';
 
-const headers = document.querySelectorAll('thead>th');
+const headers = document.querySelectorAll('thead>tr>th');
 
 for (let i = 0; i < headers.length; i++) {
   const th = headers[i];
 
-  th.addEventListener('click', () => {sortTable(i);
+  th.addEventListener('click', () => {
+    sortTable(i);
   });
 }
 
 function sortTable(ind) {
   const table = document.querySelector('table');
 
-  const rows = table.querySelectorAll('tbody>tr');
+  const rows = [...table.querySelectorAll('tbody>tr')];
 
-  rows.sort(function(rowA, rowB) {
-    const cellA = rowA.cells[ind].textContent;
-    const cellB = rowB.cells[ind].textContent;
+  Array.from(rows).sort(function(rowA, rowB) {
+    const cellA = rowA.cells[ind].innerText;
+    const cellB = rowB.cells[ind].innerText;
 
     return cellA.localeCompare(cellB);
-  })
-
-  rows.forEach(function(row) {
-    table.querySelector('tbody').appendChild(row);
   });
+
+  table.querySelector('tbody').append(...rows);
 }
